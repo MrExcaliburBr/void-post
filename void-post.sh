ef7bba1eaea1fd125712e410aa190ff92f6abb5b
MrExcaliburBr <58449046+MrExcaliburBr@users.noreply.github.com>
2020-06-15 00:46:25 -0300

    test

--- void-post.sh
+++ void-post.sh
@@ -26,51 +26,51 @@
 ##actual script
 #getting stuff 
 
-mkdir ~/.config
-mkdir ~/.local
-mkdir ~/.cache
+#mkdir ~/.config
+#mkdir ~/.local
+#mkdir ~/.cache
 
 xbps-install -S curl xrandr xorg-minimal xorg-fonts mesa-dri intel-video-accel scc libXinerama-devel libXft-devel freetype-devel libX11-devel ncurses-term qutebrowser youtube-dl dunst sxhkd unclutter zsh cmus maim feh fzf nnn vim mpv font-cozette font-awesome font-awesome5 fontconfig setxkbmap picom task htop
 
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.config/qutebrowser/config.py --create-dirs -o ~/.config/qutebrowser/config.py
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.config/qutebrowser/config.py --create-dirs -o ~/.config/qutebrowser/config.py
 
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.config/dunst/dunstrc --create-dirs -o ~/.config/dunst/dunstrc
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.config/dunst/dunstrc --create-dirs -o ~/.config/dunst/dunstrc
 
-mkdir ~/.config/sxhkd
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.config/sxhkd/sxhkdrc --create-dirs -o ~/.config/sxhkd/sxhkdrc
+#mkdir ~/.config/sxhkd
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.config/sxhkd/sxhkdrc --create-dirs -o ~/.config/sxhkd/sxhkdrc
 
-mkdir ~/.config/picom
-cat /etc/xdg/picom.conf >> ~/.config/picom/picom.conf
+#mkdir ~/.config/picom
+#cat /etc/xdg/picom.conf >> ~/.config/picom/picom.conf
 
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.config/kaomoji/kaomoji --create-dirs -o ~/.config/kaomoji
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.config/kaomoji/kaomoji --create-dirs -o ~/.config/kaomoji
 
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.zshrc --create-dirs -o ~/.zshrc
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.zshrc --create-dirs -o ~/.zshrc
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.vimrc --create-dirs -o ~/.vimrc
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/.vimrc --create-dirs -o ~/.vimrc
 
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/code/scripts/dwm-bar.sh --create-dirs -o ~/code/scripts/dwm-bar.sh
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/code/scripts/quicksearch.sh -o ~/code/scripts/quicksearch.sh
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/code/scripts/prompt.sh -o ~/code/scripts/prompt.sh
-curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/code/scripts/fuzzyfinder.sh -o ~/code/scripts/fuzzyfinder.sh
-chmod +x ~/code/scripts/dwm-bar.sh
-chmod +x ~/code/scripts/quicksearch.sh
-chmod +x ~/code/scripts/prompt.sh
-chmod +x ~/code/scripts/fuzzyfinder.sh
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/code/scripts/dwm-bar.sh --create-dirs -o ~/code/scripts/dwm-bar.sh
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/code/scripts/quicksearch.sh -o ~/code/scripts/quicksearch.sh
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/code/scripts/prompt.sh -o ~/code/scripts/prompt.sh
+#curl https://raw.githubusercontent.com/MrExcaliburBr/My-Dotfiles/master/code/scripts/fuzzyfinder.sh -o ~/code/scripts/fuzzyfinder.sh
+#chmod +x ~/code/scripts/dwm-bar.sh
+#chmod +x ~/code/scripts/quicksearch.sh
+#chmod +x ~/code/scripts/prompt.sh
+#chmod +x ~/code/scripts/fuzzyfinder.sh
 
-mkdir ~/.local/share/task
-touch ~/.local/share/taskrc
-mkdir ~/.config/less
-touch ~/.config/less/lesskey
-mkdir ~/.local/share/zsh/
-touch ~/.local/share/zsh/history
+#mkdir ~/.local/share/task
+#touch ~/.local/share/taskrc
+#mkdir ~/.config/less
+#touch ~/.config/less/lesskey
+#mkdir ~/.local/share/zsh/
+#touch ~/.local/share/zsh/history
 
-mkdir ~/.config/suckless
-cd ~/.config/suckless
-git clone https://github.com/bakkeby/dwm-flexipatch
-git clone https://github.com/bakkeby/st-flexipatch
-git clone https://git.suckless.org/dmenu
+#mkdir ~/.config/suckless
+#cd ~/.config/suckless
+#git clone https://github.com/bakkeby/dwm-flexipatch
+#git clone https://github.com/bakkeby/st-flexipatch
+#git clone https://git.suckless.org/dmenu
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
