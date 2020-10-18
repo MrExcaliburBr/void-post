bec8140e8885ccbb6ea3a0278e246f416c5be29c
MrExcaliburBr <joseeduardo3224@gmail.com>
2020-10-18 19:26:52 -0300

    bruh

--- void-post.sh
+++ void-post.sh
@@ -26,7 +26,7 @@ mkdir ~/.local/share/gnupg
 mkdir ~/.local/share/pass
 
 #installing needed packages (or just what i use ;) )
-sudo xbps-install -Sy gnupg pass passmenu ueberzug tlp ffmpeg sox alsa-tools xdg-utils font-awesome5 xclip tmux zathura zathura-pdf-poppler acpid newsboat xsetroot tree alsa-lib-devel NetworkManager alsa-utils dbus patch curl xrandr xorg-minimal xorg-fonts xf86-input-evdev xf86-input-synaptics xf86-video-fbdev xf86-video-intel mesa mesa-dri intel-video-accel gcc libXinerama-devel libXft-devel freetype-devel libX11-devel ncurses-term qutebrowser youtube-dl dunst sxhkd unclutter zsh cmus maim feh fzf ranger vim-x11 mpv fontconfig-devel setxkbmap picom task htop xbacklight slock make pkg-config xf86-video-intel xf86-input-libinput font-cozette
+sudo xbps-install -Sy nnn gnupg pass passmenu ueberzug tlp ffmpeg sox alsa-tools xdg-utils font-awesome5 xclip tmux zathura zathura-pdf-poppler acpid newsboat xsetroot tree alsa-lib-devel NetworkManager alsa-utils dbus patch curl xrandr xorg-minimal xorg-fonts xf86-input-evdev xf86-input-synaptics xf86-video-fbdev xf86-video-intel mesa mesa-dri intel-video-accel gcc libXinerama-devel libXft-devel freetype-devel libX11-devel ncurses-term qutebrowser youtube-dl dunst sxhkd unclutter zsh cmus maim feh fzf ranger vim-x11 mpv fontconfig-devel setxkbmap picom task htop xbacklight slock make pkg-config xf86-video-intel xf86-input-libinput font-cozette straw viewer
 
 #adding configs
 git clone https://github.com/MrExcaliburBr/voidrice
@@ -40,27 +40,26 @@ git clone https://github.com/MrExcaliburBr/scripts ~/code/scripts
 
 #suckless stuff
 mkdir ~/.config/suckless
-mkdir .config/suckless/dwm
-mkdir .config/suckless/st
-mkdir .config/suckless/dmenu
+cd ~/.config/suckless
 git clone https://github.com/MrExcaliburBr/my-dwm
 git clone https://github.com/MrExcaliburBr/my-st
 git clone https://git.suckless.org/dmenu
 
-cd .config/suckless/dwm
+cd .config/suckless/my-dwm
 sudo make install
-cd ~/.config/suckless/st
+cd ~/.config/suckless/my-st
 sudo make install
 cd ~/.config/suckless/dmenu
 sudo make install 
+cd ~
 
 # sound
-sudo cp void-post/asound.conf /etc/
+mv asoundrc .asoundrc
+cp void-post/.asound .
 
 #Enabling alsa, dbus, networkmanager and acpid 
 sudo ln -s /etc/sv/alsa/ /var/service/
 sudo ln -s /etc/sv/dbus/ /var/service/
 sudo unlink /etc/sv/dhcpcd/
 sudo ln -s /etc/sv/NetworkManager/ /var/service/
-
 
