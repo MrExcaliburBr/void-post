172bc45f21575242b83c821e10c102da1fd5eb1c
MrExcaliburBr <joseeduardo3224@gmail.com>
2020-12-22 21:23:03 -0300

    bruh

--- void-post.sh
+++ void-post.sh
@@ -26,14 +26,23 @@ mkdir ~/.local/share/gnupg
 mkdir ~/.local/share/pass
 
 #installing needed packages (or just what i use ;) )
-sudo xbps-install -Sy nnn gnupg pass passmenu ueberzug tlp ffmpeg sox alsa-tools xdg-utils font-awesome5 xclip tmux zathura zathura-pdf-poppler acpid newsboat xsetroot tree alsa-lib-devel NetworkManager alsa-utils dbus patch curl xrandr xorg-minimal xorg-fonts xf86-input-evdev xf86-input-synaptics xf86-video-fbdev xf86-video-intel mesa mesa-dri intel-video-accel gcc libXinerama-devel libXft-devel freetype-devel libX11-devel ncurses-term qutebrowser youtube-dl dunst sxhkd unclutter zsh cmus maim feh fzf ranger vim-x11 mpv fontconfig-devel setxkbmap picom task htop xbacklight slock make pkg-config xf86-video-intel xf86-input-libinput font-cozette straw viewer
+sudo xbps-install -Sy voidr-repo-nonfree openssh rtl8822bu-dkms flatpak gnupg pass passmenu ueberzug tlp ffmpeg sox alsa-tools xdg-utils font-awesome5 xclip tmux zathura zathura-pdf-poppler acpid newsboat xsetroot tree alsa-lib-devel NetworkManager alsa-utils dbus patch curl xrandr xorg-minimal xorg-fonts xf86-input-evdev xf86-input-synaptics xf86-video-fbdev xf86-video-intel mesa mesa-dri intel-video-accel gcc libXinerama-devel libXft-devel freetype-devel libX11-devel ncurses-term qutebrowser youtube-dl dunst sxhkd unclutter zsh cmus maim feh fzf ranger neovim mpv fontconfig-devel setxkbmap picom task htop xbacklight slock make pkg-config xf86-video-intel xf86-input-libinput font-cozette straw-viewer
+
+#flatpak repo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #adding configs
 git clone https://github.com/MrExcaliburBr/voidrice
-rm -rf voidrice/config/suckless/
+mv voidrice/config/ssh voidrice/config/.ssh
 mv voidrice/config/* .config
+
+#vim plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#tmux plugin
+mkdir .config/tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/
 
 #scripts
 git clone https://github.com/MrExcaliburBr/scripts ~/code/scripts
@@ -54,8 +63,9 @@ sudo make install
 cd ~
 
 # sound
-mv asoundrc .asoundrc
-cp void-post/.asound .
+mv voidrice/asoundrc .asoundrc
+cp voidrice/.asoundrc .
+sudo cp voidrice/xinitrc /etc/X11/xinit/
 
 #Enabling alsa, dbus, networkmanager and acpid 
 sudo ln -s /etc/sv/alsa/ /var/service/
